3c630fd5eba56437469e44556d73a295442515c2
Jacob Waters <jkwaters@gmail.com>
2017-12-03 00:51:01 -0500

    real dealio 👌

--- shift.py
+++ shift.py
@@ -1,5 +1,16 @@
 import random
 
+def inputNumber(message):
+  while True:
+    try:
+       userInput = int(input(message))
+    except ValueError:
+       print("Not an integer! Try again.")
+       continue
+    else:
+       return userInput
+       break
+
 def shift(position, distance, n):
     distance = random.choice([-1, 1]) * distance
     if (position + distance) <= 0: # (position + distance) result is negative and we 
@@ -15,20 +26,12 @@ def shift(position, distance, n):
 # print(shift(1, 1, 10))
 # print(shift(10, -1, 10))
 
-def inputNumber(message):
-  while True:
-    try:
-       userInput = int(input(message))
-    except ValueError:
-       print("Not an integer! Try again.")
-       continue
-    else:
-       return userInput
-       break
+
+
 
 iterations = 0
 n = 10 # inputNumber("How many nodes? ")
-numRobots = 3 # inputNumber("How many robots? ")
+numRobots = 5 # inputNumber("How many robots? ")
 robots = set(random.sample(range(1, n+1), numRobots))
 print(robots)
 
@@ -37,7 +40,7 @@ while len(robots) > 1:
     robotsBefore = robots
     robotsAfter = set()
 
-    while len(robotsBefore) > 1 or not robots.issubset(robotsAfter):
+    while len(robotsBefore) > 0:
         robotsAfter.add(shift(robotsBefore.pop(),1,n))
         robotsAfter.difference_update(robotsBefore)
         print(robotsBefore, robotsAfter)
@@ -45,11 +48,5 @@ while len(robots) > 1:
     print(robotsAfter, ' : ', iterations)
     
     robots = robotsAfter
-    # robotsAftered = robotsAftered.difference(robotsAftered.intersection(robots)) # doesn't work with 3 numbers shifting, need to find a pair within +/- 1
     if iterations >= 50:
         break
-
-# robotsAfter = set()
-# while len(robots) > 1 and not robots.issubset(robotsAfter):
-#     robotsAfter.add(shift(robots.pop(),1,n))
-#     print(robots, robotsAfter)
