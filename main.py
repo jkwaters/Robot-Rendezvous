46f1a1bcd193131edb0026d282d15fbb169a2560
Jacob Waters <jkwaters@gmail.com>
2017-12-05 18:17:18 -0500

    seperated code into better functions

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import Robot
-import Cycle
+import shift
 
-# Ask how many nodes
-# Ask how many robots
+tests = 1000
+nodes = 10 # shift.inputNumber("How many nodes? ")
+# numRobots = 10 # shift.inputNumber("How many robots? ")
 # Ask movement specifics
+
+for numRobots in range(int(nodes/10),nodes+1,int(nodes/10)):
+    totalIterations = 0
+    totalShifts = 0
+    for i in range(0,tests):
+        result = shift.rendezvouz(nodes, numRobots)
+        totalIterations += result[0]
+        totalShifts += result[1]
+        # print(robots, ' : ', totalIterations)
+    avgIterations = totalIterations / tests
+    avgShifts = totalShifts / tests
+    print (numRobots, avgIterations, avgShifts)
--- shift.py
+++ shift.py
@@ -1,15 +1,15 @@
 import random
 
 def inputNumber(message):
-  while True:
-    try:
-       userInput = int(input(message))
-    except ValueError:
-       print("Not an integer! Try again.")
-       continue
-    else:
-       return userInput
-       break
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return userInput
+        break
 
 def shift(position, distance, n):
     if (position + distance) <= 0: # (position + distance) result is negative and we 
@@ -19,37 +19,37 @@ def shift(position, distance, n):
     else:
         return (position + distance)
 
+
+
+def rendezvouz(nodes, numRobots):
+    iterations = 0
+    shifts = 0
+    robots = set(random.sample(range(1, nodes+1), numRobots))
+    # print(robots)
+
+    while len(robots) > 1:
+        iterations += 1
+        lenBefore = len(robots)
+        robotsBefore = set(robots)
+        robotsAfter = set()
+        moves = list()
+
+        while len(robotsBefore) > 0:
+            move = random.choice([-1, 1]) * random.randint(1,int(nodes/lenBefore)) 
+            moves.append(move)
+            robotsAfter.add(shift(robotsBefore.pop(),move,nodes))
+            shifts += 1
+            robotsAfter.difference_update(robotsBefore)
+
+        # if len(robotsAfter) != lenBefore:
+            # print("before", robots)
+            # print("move: ", moves)
+            # print(robotsAfter, ' : ', len(robotsAfter), ' : ', iterations)
+        robots = set(robotsAfter)
+    return (iterations, shifts)
+
 # test edge cases & basic sanity testing
 # print(shift(1, -1, 10))
 # print(shift(10, 1, 10))
 # print(shift(1, 1, 10))
 # print(shift(10, -1, 10))
-
-
-
-
-iterations = 0
-n = 100 # inputNumber("How many nodes? ")
-numRobots = 99 # inputNumber("How many robots? ")
-robots = set(random.sample(range(1, n+1), numRobots))
-print(robots)
-
-while len(robots) > 1:
-    iterations += 1
-    lenBefore = len(robots)
-    robotsBefore = set(robots)
-    robotsAfter = set()
-    moves = list()
-
-    while len(robotsBefore) > 0:
-        move = random.choice([-1, 1]) # * random.randint(1,int(n/lenBefore))
-        moves.append(move)
-        robotsAfter.add(shift(robotsBefore.pop(),move,n))
-        robotsAfter.difference_update(robotsBefore)
-        # print(robotsBefore, robotsAfter)
-
-    if len(robotsAfter) != lenBefore:
-         print("before", robots)
-         print("move: ", moves)
-         print(robotsAfter, ' : ', len(robotsAfter), ' : ', iterations)
-    robots = set(robotsAfter)
